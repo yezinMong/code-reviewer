75e17d6b04b29ef146ae2bf039f8a167a1cca92f
dayae001 <dayae001@naver.com>
2024-06-12 15:46:04 +0900

    getdivide commit

--- calc/calc.py
+++ calc/calc.py
@@ -1,17 +1,22 @@
 def getSum(a, b):
     pass
 
+
 def getGop(a, b):
     pass
+
 
 def getZegop(a):
     pass
 
+
 def getMinus(a, b):
     pass
 
+
 def getDivide(a, b):
-    pass
+    return 0 if b == 0 else a / b
+
 
 def getSumSum(a, b, c):
     pass
